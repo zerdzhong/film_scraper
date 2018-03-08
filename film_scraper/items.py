9fe34a54d3ac438c6db485cab9ddf91acfa7a8a5
zerdzhong <zhongzhendongchn@gmail.com>
2018-03-08 22:40:56 +0800

    Refactor film items

--- film_scraper/items.py
+++ film_scraper/items.py
@@ -8,20 +8,31 @@
 from scrapy import Item, Field
 
 
-class DoubanFilmItem(Item):
+class DoubanFilmBaseItem(Item):
     # ID
     id = Field()
     # 名称
     name = Field()
+
+
+class DoubanFilmItem(DoubanFilmBaseItem):
+    # 导演
+    director = Field()
+    # 编剧
+    writer = Field()
+    # 演员
+    actor = Field()
     # 缩略图地址
     post_img_url = Field()
     # 详情地址
     detail_url = Field()
     # 评分
     rate = Field()
+    # 评分人数
+    rate_sum = Field()
 
 
-class DoubanComingFilmItem(DoubanFilmItem):
+class DoubanComingFilmItem(DoubanFilmBaseItem):
     # 想看
     wish_watch_count = Field()
     # 上映日期
