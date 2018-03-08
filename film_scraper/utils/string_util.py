ae40a7bb9fcd140250b6a4316a1d20fe1b7a0e7b
zerdzhong <zhongzhendongchn@gmail.com>
2018-03-08 22:52:30 +0800

    add string util methods

--- film_scraper/utils/string_util.py
+++ film_scraper/utils/string_util.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
 
-def is_valid_string(s):
+import re
+
+SPECIAL_CHAR_RE = "[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）：]+"
+POSITIVE_NUMBER_RE = "\d+"
+
+
+def valid_string(s):
     return s and s.strip()
+
+
+def valid_name(s):
+    s = re.sub(SPECIAL_CHAR_RE, "", s)
+    return s
+
+
+def valid_positive_number(s):
+    return re.match(POSITIVE_NUMBER_RE, s)
+
+
