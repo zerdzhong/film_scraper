6d4a6aa5c5a04bb3662c05ae4ca53d333cd8ad09
zerdzhong <zhongzhendongchn@gmail.com>
2018-03-15 15:36:33 +0800

    VSCode debug config

--- film_scraper/run.py
+++ film_scraper/run.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from scrapy import cmdline
+import os
 
 local_splash_url = 'http://localhost:8050'
+local_mongo_url = 'mongodb://localhost:27017'
 
-name = 'douban_film_spider'
-cmd = 'scrapy crawl {0} -s SPLASH_URL={1} -o test.csv'.format(name, local_splash_url)
-cmdline.execute(cmd.split())
+if __name__ == '__main__':
+    docker_cmd = 'docker container start film_scraper_mongodb'
+    os.system(docker_cmd)
+
+    name = 'douban_film_spider'
+    cmd = 'scrapy crawl {0} -s MONGO_URI={1} '.format(name, local_mongo_url)
+    cmdline.execute(cmd.split())
